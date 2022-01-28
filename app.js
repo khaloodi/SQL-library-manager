547006d8c29264c240316e69685c8c4b96bf550d
Khaled Adad <khaledadad@lewisu.edu>
2022-01-28 12:56:05 -0700

    removed the books router and everything going through index router

--- app.js
+++ app.js
@@ -25,8 +25,8 @@ var path = require('path');
 var cookieParser = require('cookie-parser');
 var logger = require('morgan');
 
-const routes = require('./routes/index');
-const books = require('./routes/books');
+const indexRouter = require('./routes/index');
+const usersRouter = require('./routes/users');
 
 var app = express();
 
@@ -40,8 +40,8 @@ app.use(express.urlencoded({ extended: false }));
 app.use(cookieParser());
 app.use(express.static(path.join(__dirname, 'public')));
 
-app.use('/', routes);
-app.use('/books', books);
+app.use('/', indexRouter);
+app.use('/users', usersRouter);
 
 // catch 404 and forward to error handler
 app.use(function(req, res, next) {
--- routes/index.js
+++ routes/index.js
@@ -1,9 +1,32 @@
 const express = require('express');
 const router = express.Router();
+//requiring book model to be able to use it and all associated ORM methods(CRUD)
+const Book = require('../models').Book;
 
-/* GET home page. */
+/* Handler function to wrap each route. In other words, we are abstracting away the try catch for all of our calls by creating an asyncHandler function and using or implimenting it in all of our calls.*/
+function asyncHandler(cb) { // catches an error (or a rejected promise) that occurs in a route and forwards the error to the global error handler (in app.js) with the next() method
+    return async(req, res, next) => {
+        try {
+            await cb(req, res, next)
+        } catch (error) {
+            // res.status(500).send(error) // this line/catch statement will send a 500 error or internal server error to the user if an exception is thrown in the try block, or in any of the handler functions           
+            // With only next(error) inside of the catch block, if you visit an article that does not exist in the database (/articles/101, for example), or run findByPk() with an invalid ID, the response returns errors and a 500 status code. The server cannot process the request because the entry is not found.
+            next(error); // Forward error to the global error handler, I believe this operates the same as res.status(500).send(error)
+
+        }
+    }
+}
+
+/* GET home Route. */
 router.get('/', (req, res, next) => {
   res.redirect("/books")
 });
+
+/* GET Books. */
+router.get('/books', asyncHandler(async(req, res) => {
+  // res.render('index', { title: 'Express' });
+  const books = await Book.findAll()
+  res.render('books/books', { books, title: "Books" })
+}));
 
 module.exports = router;
